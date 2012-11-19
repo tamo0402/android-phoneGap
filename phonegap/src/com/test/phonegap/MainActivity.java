86245f62ca233f331936c48dabe2d733123cdc14
tamo0402 <maximum.saboten@gmail.com>
2012-11-19 15:42:13 +0900

    Hello World!!

---
+++ phonegap/src/com/test/phonegap/MainActivity.java
@@ -0,0 +1,22 @@
+package com.test.phonegap;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
